b135323d1dae56ab1f57b9cd2cd66876fbcccb94
Natarajan <nataraj.itsolution@gmail.com>
2024-02-21 23:44:25 +0530

    standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MasterService } from './service/master.service';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NgxMaskDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'standalone';
+
+  constructor(private service:MasterService){
+
+  }
+  ngOnInit(): void {
+     this.service.Getall().subscribe(item=>{
+      console.log(item);
+     })
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './service/data.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideHttpClient(),
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(DataService)),
+    provideEnvironmentNgxMask()
+  ]
 };
